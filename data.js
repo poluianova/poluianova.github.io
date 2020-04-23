cc5145845e7a61b50ca5b44e1d2eefcb216cfd88
marina <marinka0588@ya.ru>
2020-04-23 14:27:19 +0300

    some data changed

--- data.js
+++ data.js
@@ -1,67 +1,62 @@
 [
-    {
-		"date": "2020-03-27",
-		"hits": 2,
-		"unique": 1,
-		"registrations": 4,
-		"demo_registrations": 5,
-		"conversion": 4,
-		"deposit": 5,
-		"ftd": 3,
-		"deals": 4,
-		"profit": 50
-
-    },
-    {
-		"date": "2020-04-01",
-		"hits": 8,
-		"unique": 8,
-		"registrations": 8,
-		"demo_registrations": 7,
-		"conversion": 7,
-		"deposit": 6,
-		"ftd": 6,
-		"deals": 7,
-		"profit": 10
-
-    },
-    {
-		"date": "2020-03-31",
-		"hits": 4,
-		"unique": 0,
-		"registrations": 2,
-		"demo_registrations": 9,
-		"conversion": 7,
-		"deposit": 3,
-		"ftd": 8,
-		"deals": 2,
-		"profit": 210
-
-    },
-    {
-		"date": "2020-04-20",
-		"hits": 5,
-		"unique": 5,
-		"registrations": 4,
-		"demo_registrations": 3,
-		"conversion": 0,
-		"deposit": 7,
-		"ftd": 8,
-		"deals": 7,
-		"profit": 200
-
-	},
-	{
-		"date": "2020-05-01",
-		"hits": 4,
-		"unique": 1,
-		"registrations": 0,
-		"demo_registrations": 2,
-		"conversion": 7,
-		"deposit": 3,
-		"ftd": 1,
-		"deals": 8,
-		"profit": 150
-
-    }
-]
+  {
+    date: "2020-04-27",
+    hits: 2,
+    unique: 1,
+    registrations: 4,
+    demo_registrations: 5,
+    conversion: 4,
+    deposit: 5,
+    ftd: 3,
+    deals: 4,
+    profit: 50,
+  },
+  {
+    date: "2020-04-01",
+    hits: 8,
+    unique: 8,
+    registrations: 8,
+    demo_registrations: 7,
+    conversion: 7,
+    deposit: 6,
+    ftd: 6,
+    deals: 7,
+    profit: 10,
+  },
+  {
+    date: "2020-05-31",
+    hits: 4,
+    unique: 0,
+    registrations: 2,
+    demo_registrations: 9,
+    conversion: 7,
+    deposit: 3,
+    ftd: 8,
+    deals: 2,
+    profit: 210,
+  },
+  {
+    date: "2020-05-20",
+    hits: 5,
+    unique: 5,
+    registrations: 4,
+    demo_registrations: 3,
+    conversion: 0,
+    deposit: 7,
+    ftd: 8,
+    deals: 7,
+    profit: 200,
+  },
+  {
+    date: "2020-06-01",
+    hits: 4,
+    unique: 1,
+    registrations: 0,
+    demo_registrations: 2,
+    conversion: 7,
+    deposit: 3,
+    ftd: 1,
+    deals: 8,
+    profit: 150,
+  },
+];
